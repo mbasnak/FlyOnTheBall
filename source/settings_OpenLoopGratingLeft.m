d45c1a9d1b76d15620c130410d51f8313d99f30c
Melanie Basnak <melaniebasnak@g.harvard.edu>
2018-09-28 11:27:15 -0400

    Changed the position function in the gratings settings to match the correct one in the SD card.

--- source/settings_OpenLoopGratingLeft.m
+++ source/settings_OpenLoopGratingLeft.m
@@ -6,5 +6,5 @@ niOI.DurationInSeconds = 0.5;
 Panel_com('set_pattern_id', 4); %the pattern 4 are the gratings
 Panel_com('set_mode', [4 4]); %set the mode to be controlled by an outside function
 Panel_com('set_position',startPos); %we should run this if we want to stimuli to start centered (doesn't make a lot of sense for the gratings though)
-Panel_com('set_posfunc_id',[1 4]);
-Panel_com('set_posfunc_id',[2 4]);
+Panel_com('set_posfunc_id',[1 2]); %This function moves the pattern to the left at 20 Hz
+Panel_com('set_posfunc_id',[2 2]);
