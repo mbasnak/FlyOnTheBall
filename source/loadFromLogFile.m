802376342af0494be2d60df5543ea08e0330b345
Melanie Basnak <melaniebasnak@g.harvard.edu>
2020-03-18 16:57:13 -0400

    I was having issues loading the data and changed the argument 'r' to 'r+' to see if it solved things. It didn't really change anything but the problem was elsewhere and got solved.

--- source/loadFromLogFile.m
+++ source/loadFromLogFile.m
@@ -1,5 +1,6 @@
 function [daq_data] = loadFromLogFile(log_file_path,n_chan)
-    log_file_id = fopen(log_file_path,'r');
+    %log_file_id = fopen(log_file_path,'r');
+    log_file_id = fopen(log_file_path,'r+'); %changed on 20190917 to see if it works better for the virtual hallway
     daq_data  = fread(log_file_id,'double');
     daq_data  = reshape(daq_data,n_chan+1,[]);
     daq_data  = daq_data(2:end,:);
