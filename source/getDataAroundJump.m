01333bd0b4840f4eb929038f3eb41f56cb6dc619
Melanie Basnak <melaniebasnak@g.harvard.edu>
2020-03-18 16:55:31 -0400

    I added the sampling rate of the NiDaq as a variable (samplingRate) to be used as input in different calculations.

--- source/getDataAroundJump.m
+++ source/getDataAroundJump.m
@@ -1,4 +1,4 @@
-function [DATA] = getDataAroundJump(data,jumpFrames,aroundJumpSec,sizeBall)
+function [DATA] = getDataAroundJump(data,jumpFrames,aroundJumpSec,sizeBall,samplingRate)
 
 %This function outputs a data structure for position and velocity around
 %the bar jumps
@@ -13,9 +13,9 @@ function [DATA] = getDataAroundJump(data,jumpFrames,aroundJumpSec,sizeBall)
 for i = 1:length(jumpFrames)
     
     %x position and forward walking
-    aroundJumpIntx(:,i) = data(jumpFrames(i)-aroundJumpSec*1000:jumpFrames(i)+aroundJumpSec*1000,3);
+    aroundJumpIntx(:,i) = data(jumpFrames(i)-aroundJumpSec*samplingRate:jumpFrames(i)+aroundJumpSec*samplingRate,3);
     %The - sign is necessary to invert the x axis.
-    downsampled.aJ(:,i) = downsample(aroundJumpIntx(:,i),1000/25);
+    downsampled.aJ(:,i) = downsample(aroundJumpIntx(:,i),samplingRate/25);
     downsRad.aJ(:,i) = downsampled.aJ(:,i) .* 2 .* pi ./ 10;
     unwrapped.aJ(:,i) = unwrap(downsRad.aJ(:,i));
     smoothed.aJ(:,i) = smoothdata(unwrapped.aJ(:,i),'rlowess',25); 
@@ -24,8 +24,8 @@ for i = 1:length(jumpFrames)
     DATA.forwardVel(:,i) = smoothdata(diff.aJ(:,i),'rlowess',15);
     
     %angular velocity
-    aroundJumpa(:,i) = data(jumpFrames(i)-aroundJumpSec*1000:jumpFrames(i)+aroundJumpSec*1000,1);
-    downsampled.aJa(:,i) = downsample(aroundJumpa(:,i),1000/25);
+    aroundJumpa(:,i) = data(jumpFrames(i)-aroundJumpSec*samplingRate:jumpFrames(i)+aroundJumpSec*samplingRate,1);
+    downsampled.aJa(:,i) = downsample(aroundJumpa(:,i),samplingRate/25);
     downsRad.aJa(:,i) = downsampled.aJa(:,i) .* 2 .* pi ./ 10;
     unwrapped.aJa(:,i) = unwrap(downsRad.aJa(:,i));
     smoothed.aJa(:,i) = smoothdata(unwrapped.aJa(:,i),'rlowess',25); 
